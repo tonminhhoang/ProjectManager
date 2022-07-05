bd09ff653d3aa79f29f3a4dede462318a7eec58e
hoangharrytm <97387013+hoangharrytm@users.noreply.github.com>
2022-07-05 15:17:54 +0700

    Delete welcome_page.py

--- welcome_page.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Welcome:
-	def __init__(self):
-		self.logo = pygame.image.load("Logo.png")
-		self.welcome = pygame.image.load("welcome.png")
-
-	def show_image(self):
-		pass
