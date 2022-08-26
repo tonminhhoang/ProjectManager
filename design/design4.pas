53bb40ede4eb2ddb5d1b161813ba1c268ae1ed61
hoangharrytm <tonminhhoang2006@gmail.com>
2022-08-26 08:21:28 +0700

    update code & add login function

---
+++ design/design4.pas
@@ -0,0 +1,21 @@
+program Design3;
+
+uses crt;
+
+BEGIN
+	Writeln(' _    ');
+	Writeln('| |   ');
+	Writeln('| |   ');
+	Writeln('| |__ ');
+	Writeln('|____|');
+
+	Writeln ('----------------------------------');
+	
+	Writeln(' _    ');
+	Write('|'); TextBackground(Red); Write(' '); TextBackground(Black); Writeln('|');
+	Writeln('| |   ');
+	Writeln('| |   ');
+	Writeln('| |__ ');
+	Writeln('|____|');
+	Readln();
+END.
--- login.py
+++ login.py
@@ -15,31 +15,31 @@ client = gspread.authorize(creds)
 spreadsheet = client.open("Konst - Project Manager")
 worksheet = spreadsheet.sheet1
 num_of_user = int(worksheet.acell('C2').value)
-get_user_value = worksheet.col_values(1)
-del get_user_value[0]
+get_user_value = [worksheet.col_values(1),worksheet.col_values(2)]
+
+del(get_user_value[0][0], get_user_value[1][0])
 
 read_file = open("data.txt", "r")
 data = read_file.readline().strip('\n').split(" ")
 read_file.close()
 
-# print(data)
+'''
+print(data)
 
-# for i in range(0,num_of_user):
-# 	if data[0] == get_user_value[i]:
-# 		print("yes!")
-# 		break
-# 	elif i == num_of_user-1:
-# 		print(i)
-# 		print("no")
+for i in range(0,num_of_user):
+	if data[0] == get_user_value[0][i] and data[1] == get_user_value[1][i]:
+		print("yes!")
+		break
+	elif i == num_of_user-1:
+		print(i)
+		print("no")
+'''
 
 result = open("data.txt", "w")
 for check_user in range(0, num_of_user):
-	if data[0] == get_user_value[check_user]:
-		result.write("Username already exist!")
+	if data[0] == get_user_value[0][check_user] and data[1] == get_user_value[1][check_user]:
+		result.write("Login success!")
 		break
-	elif check_user == (num_of_user-1):
-		result.write("Success!")
-		worksheet.update_cell(num_of_user+2, 1, data[0])
-		worksheet.update_cell(num_of_user+2, 2, data[1])
-		worksheet.update_acell('C2', num_of_user+1)
+	elif check_user == num_of_user-1:
+		result.write("Login failed!")
 result.close()
